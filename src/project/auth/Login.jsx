eafe07e33cc0aebb5ee538e8fa2d6ffcf438b72e
Rohan <fynorm2003@gmail.com>
2025-08-21 22:33:40 +0530

    first commit

--- src/project/auth/Login.jsx
+++ src/project/auth/Login.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>
-      <div className="login" style={{}}>
-        <h1>Login From</h1>
-        <div className="from">
-            <p style={{padding:"0",margin:"0",marginLeft:"-19vh"}}>Email</p>
-            <input type="text" placeholder='Email'/>
-            <p style={{padding:"0",margin:"0",marginLeft:"-19vh"}}>Password</p>
-            <input type="password" />
-        </div>
-        <button style={{marginTop:"2vh"}}>Login</button>
-        <a href="/register">Signup</a>
+    <div className='l'>
+      <h1 className='login'>
+        LOGIN PAGE
+      </h1>
+      <br />
+      <p className='details'>
+        ENTER YOUR CREDENTIALS TO LOGIN !
+      </p>
+      <br />
+      <div className='users'>
+        <p className='us'>ENTER YOUR USERNAME : </p>
+        
+      <input className='u' type="username" placeholder='USERNAME' />
       </div>
+      
+      <div className='pas'>
+        <p className='pa'>ENTER YOUR PASSWORD : </p>
+      <input className='py' type="password" placeholder='PASSWORD' />
+      </div>
+      <div className='but'>
+      <button className='b' type="submit">LOGIN</button>
+      </div>
+      <br />
+
+      <a className='a' href="/register">SIGN UP ! DONT HAVE ACCOUNT?</a>
+      
+     
     </div>
   )
 }
--- src/project/auth/Register.jsx
+++ src/project/auth/Register.jsx
@@ -1,24 +1,44 @@
 import React from 'react'
+import './Register.css'
 
 const Register = () => {
   return (
-    <div>
-        <div>
-      <div className="Signup" style={{}}>
-        <h1>SignUp From</h1>
-        <div className="from">
-            <p style={{padding:"0",margin:"0",marginLeft:"-19vh"}}>Name</p>
-            <input type="text" placeholder='Name'/>
-            <p style={{padding:"0",margin:"0",marginLeft:"-19vh"}}>Email</p>
-            <input type="text" placeholder='Email'/>
-            <p style={{padding:"0",margin:"0",marginLeft:"-16vh"}}>Password</p>
-            <input type="password" />
-             
-        </div>
-        <button style={{marginTop:"2vh"}}>SignUp</button>
-        <a href="/login">Login</a>
-      </div>
-    </div>
+    <div className='r'>
+       <h1 className='REGISTER'>REGISTER PAGE</h1>
+       <br />
+       <p className='para'>
+        ENTER YOUR DETAILS HERE TO SIGNUP !
+       </p>
+       <br />
+       <div className='userr'>
+        <p className='n'>ENTER YOUR USERNAME OR NAME :</p>
+        <input className='userss' type="username" placeholder='USERNAME' />
+       </div>
+       
+       <div className='phone'>
+        <p className='phn'>ENTER YOUR PHONE NUMBER :</p>
+        <input className='ph' type="number" placeholder='PHONE NUMBER' />
+       </div>
+       <br />
+       <div className='email'>
+        <p className='e'>ENTER YOUR EMAIL HERE :</p>
+        <input className='em' type="email" placeholder='EMAIL'/>
+        
+       </div>
+       <br />
+       <div className='pw'>
+        <p className='rp'>SET YOUR PASSWORD</p>
+        <input className='pr' type="password" placeholder='PASSWORD' />
+       </div>
+       <br />
+       <div className='buut'>
+        <button className='bu' type="submit">REGISTER</button>
+
+       </div>
+       <br />
+       <a className='an' href="/login">ALREADY HAVE ACCOUNT? LOGIN NOW</a>
+
+
     </div>
   )
 }
